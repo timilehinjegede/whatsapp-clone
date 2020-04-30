1f22e66a27aafadef5a1643246b7bd73b11d9897
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-30 07:16:02 +0100

    Clean up code

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CallsScreen extends StatelessWidget {
+class CameraScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsapp_clone/provider/theme_provider.dart';
+import 'package:whatsapp_clone/screens/calls.dart';
+import 'package:whatsapp_clone/screens/camera.dart';
+import 'package:whatsapp_clone/screens/chats.dart';
+import 'package:whatsapp_clone/screens/status.dart';
+import 'package:whatsapp_clone/utils/margin.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(vsync: this, length: 4, initialIndex: 1);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(
+          'Whatsapp',
+        ),
+        centerTitle: false,
+        actions: <Widget>[
+          Icon(Icons.search),
+          XMargin(15),
+          Icon(Icons.more_vert),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: 'Chats',
+            ),
+            Tab(
+              text: 'Status',
+            ),
+            Tab(
+              text: 'Calls',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CameraScreen(),
+          ChatScreen(),
+          StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            Provider.of<ThemeProvider>(context, listen: false).changeTheme(),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes a
     );
   }
 }
