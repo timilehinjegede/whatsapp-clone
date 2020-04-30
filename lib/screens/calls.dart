82a68e25385ca46cd0c91b5b5341c80eb2a85ce9
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-30 18:43:42 +0100

    Setup the calls model and call screen

--- lib/screens/calls.dart
+++ lib/screens/calls.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/model/call_item.dart';
 
 class CallsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index){
+        return Column(
+          children: <Widget>[
+            CallItem(
+              contactName: 'Temi',
+              isMissed: true,
+              isVideo: false,
+              isCallMade: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 50),
+              child: Divider(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
