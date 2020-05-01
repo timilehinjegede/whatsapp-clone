e3c3692a46e9590a7dd9bf2da3ad7bb1a8747f2f
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-01 10:11:23 +0100

    Finally done with the calls screen

--- lib/components/call_item.dart
+++ lib/components/call_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/utils/margin.dart';
 
 class CallItem extends StatelessWidget {
-  final AssetImage image;
+  final String imagePath;
   final String contactName;
   final String date;
   final bool isVideo;
@@ -10,7 +10,12 @@ class CallItem extends StatelessWidget {
   final bool isCallMade;
 
   CallItem(
-      {this.image, this.contactName, this.date, this.isVideo, this.isMissed,this.isCallMade});
+      {this.imagePath,
+      this.contactName,
+      this.date,
+      this.isVideo,
+      this.isMissed,
+      this.isCallMade});
 
   @override
   Widget build(BuildContext context) {
@@ -19,31 +24,35 @@ class CallItem extends StatelessWidget {
         radius: 30,
         backgroundColor: Theme.of(context).accentColor,
       ),
-      title: Text('Temi'),
+      title: Text(contactName),
       subtitle: Row(
         children: <Widget>[
-          isCallMade ? Icon(
-            Icons.call_made,
-            size: 16,
-            color: isMissed ? Colors.red : Theme.of(context).accentColor,
-          ) : Icon(
-            Icons.call_received,
-            size: 16,
-            color: isMissed ? Colors.red : Theme.of(context).accentColor,
-          ),
+          isCallMade
+              ? Icon(
+                  Icons.call_made,
+                  size: 16,
+                  color: isMissed ? Colors.red : Theme.of(context).accentColor,
+                )
+              : Icon(
+                  Icons.call_received,
+                  size: 16,
+                  color: isMissed ? Colors.red : Theme.of(context).accentColor,
+                ),
           XMargin(5),
-          Text('26, April, 21:04'),
+          Text(date),
         ],
       ),
       trailing: isVideo
           ? Icon(
-        Icons.videocam,
-        color: Theme.of(context).accentColor,
-      )
+              Icons.videocam,
+              color: Theme.of(context).accentColor,
+              size: 26,
+            )
           : Icon(
-        Icons.call,
-        color: Theme.of(context).accentColor,
-      ),
+              Icons.call,
+              color: Theme.of(context).accentColor,
+              size: 22,
+            ),
     );
   }
 }
--- lib/screens/calls_screen.dart
+++ lib/screens/calls_screen.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/components/call_item.dart';
+import 'package:whatsapp_clone/model/call.dart';
 
 class CallsScreen extends StatelessWidget {
-
-  // call data
-
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (BuildContext context, int index){
+      itemCount: Call.getAllCalls.length,
+      itemBuilder: (BuildContext context, int index) {
         return Column(
           children: <Widget>[
             CallItem(
-              contactName: 'Temi',
-              isMissed: true,
-              isVideo: false,
-              isCallMade: true,
+              // no images or avatars for now
+              imagePath: Call.getAllCalls[index].imagePath,
+              // filter through the contact name of the call dummy data
+              contactName: Call.getAllCalls[index].contactName,
+              // filter through the isMissed value of the call dummy data
+              isMissed: Call.getAllCalls[index].isMissed,
+              // filter through the isVideo value of the call dummy data
+              isVideo: Call.getAllCalls[index].isVideo,
+              // filter through the isCallMade value of the call dummy data
+              isCallMade: Call.getAllCalls[index].isCallMade,
+              // filter through the date of the call dummy data
+              date: Call.getAllCalls[index].date,
             ),
             Padding(
               padding: const EdgeInsets.only(left: 50),
