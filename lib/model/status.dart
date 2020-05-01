6dfae2bb048dbb2ab7ec1447dab7c14770208885
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-01 11:11:24 +0100

    Finally done with the status screen. Thank you JESUS

--- lib/model/status.dart
+++ lib/model/status.dart
@@ -23,21 +23,13 @@ class Status {
     ),
     // model item 1
     Status(
-      contactName: 'Peace',
-      time: 'Today, 00:24',
-      imagePath: '',
-      isRecent: false,
-      isViewed: true,
-    ),
-    // model item 2
-    Status(
       contactName: 'Victor',
       time: 'Today, 09:22',
       imagePath: '',
       isRecent: false,
       isViewed: false,
     ),
-    // model item 3
+    // model item 2
     Status(
       contactName: 'Tobi',
       time: 'Today, 01:14',
@@ -45,15 +37,7 @@ class Status {
       isRecent: true,
       isViewed: false,
     ),
-    // model item 4
-    Status(
-      contactName: 'Daniel',
-      time: 'Yesterday, 23:39',
-      imagePath: '',
-      isRecent: true,
-      isViewed: false,
-    ),
-    // model item 5
+    // model item 3
     Status(
       contactName: 'Segun',
       time: 'Yesterday, 23:48',
@@ -61,7 +45,7 @@ class Status {
       isRecent: false,
       isViewed: true,
     ),
-    // model item 6
+    // model item 4
     Status(
       contactName: 'Bankole',
       time: 'Today, 08:48',
@@ -69,7 +53,7 @@ class Status {
       isRecent: false,
       isViewed: false,
     ),
-    // model item 7
+    // model item 5
     Status(
       contactName: 'David',
       time: 'Yesterday, 22:34',
@@ -77,7 +61,7 @@ class Status {
       isRecent: false,
       isViewed: true,
     ),
-    // model item 8
+    // model item 6
     Status(
       contactName: 'David',
       time: 'Today, 08:30',
@@ -85,23 +69,7 @@ class Status {
       isRecent: false,
       isViewed: false,
     ),
-    // model item 9
-    Status(
-      contactName: 'Damola',
-      time: 'Yesterday, 19:57',
-      imagePath: '',
-      isRecent: false,
-      isViewed: true,
-    ),
-    // model item 10
-    Status(
-      contactName: 'Ifeoluwa',
-      time: 'Today, 06:57',
-      imagePath: '',
-      isRecent: false,
-      isViewed: false,
-    ),
-    // model item 11
+    // model item 7
     Status(
       contactName: 'Emmanuel',
       time: 'Yesterday, 17:59',
@@ -109,15 +77,7 @@ class Status {
       isRecent: false,
       isViewed: true,
     ),
-    // model item 12
-    Status(
-      contactName: 'Joshua',
-      time: 'Today, 04:50',
-      imagePath: '',
-      isRecent: false,
-      isViewed: false,
-    ),
-    // model item 13
+    // model item 8
     Status(
       contactName: 'Julius',
       time: 'Yesterday, 17:45',
@@ -125,7 +85,7 @@ class Status {
       isRecent: true,
       isViewed: false,
     ),
-    // model item 14
+    // model item 9
     Status(
       contactName: 'Kayode',
       time: 'Yesterday, 12:29',
--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -1,81 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/components/status_item.dart';
+import 'package:whatsapp_clone/model/status.dart';
 import 'package:whatsapp_clone/utils/margin.dart';
 
 class StatusScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ListTile(
-          leading: Stack(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 30,
-                backgroundColor: Theme.of(context).accentColor,
-              ),
-              Positioned(
-                top: 35,
-                left: 38,
-                child: Container(
-                  height: 20,
-                  width: 20,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).primaryColor,
+    return ListView(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ListTile(
+              leading: Stack(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Theme.of(context).accentColor,
                   ),
-                  child: Icon(
-                    Icons.add,
-                    color: Theme.of(context).primaryIconTheme.color,
-                    size: 18,
+                  Positioned(
+                    top: 35,
+                    left: 38,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        color: Theme.of(context).primaryIconTheme.color,
+                        size: 18,
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-          title: Text(
-            'My status',
-          ),
-          subtitle: Text('Tap to add status update'),
-        ),
-        YMargin(10),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text('Recent updates'),
-        ),
-        YMargin(5),
-        StatusItem(
-          contactName: 'Temi',
-          time: 'yesterday',
-          isViewed: false,
-          isRecent: true,
-        ),
-        YMargin(10),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text('Viewed updates'),
-        ),
-        YMargin(5),
-        StatusItem(
-          contactName: 'Temi',
-          time: 'yesterday',
-          isViewed: true,
-          isRecent: false,
-        ),
-        YMargin(10),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text('Muted updates'),
-        ),
-        YMargin(5),
-        StatusItem(
-          contactName: 'Temi',
-          time: 'yesterday',
-          isViewed: false,
-          isRecent: false,
+              title: Text(
+                'My status',
+              ),
+              subtitle: Text('Tap to add status update'),
+            ),
+            YMargin(10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text('Recent updates'),
+            ),
+            YMargin(5),
+            _buildRecent(),
+            YMargin(10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text('Viewed updates'),
+            ),
+            YMargin(5),
+            _buildViewed(),
+            YMargin(10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Muted updates'),
+                  Icon(Icons.keyboard_arrow_up, size: 26, color: Theme.of(context).primaryColor,)
+                ],
+              ),
+            ),
+            YMargin(5),
+            _buildMuted(),
+          ],
         ),
       ],
+    );
+  }
+
+  // ===== BUILD RECENT UPDATES HERE =====
+  Widget _buildRecent() {
+
+    List<Status> isRecentList =  Status.getAllStatus.where((status) => status.isRecent == true).toList();
+    int length = isRecentList.length;
+
+    return Column(
+      children: List<Widget>.generate(
+        length,
+            (index) {
+          return StatusItem(
+            contactName: isRecentList[index].contactName,
+            time: isRecentList[index].time,
+            // image is empty for now
+            imagePath: isRecentList[index].imagePath,
+            isViewed: isRecentList[index].isViewed,
+            isRecent: isRecentList[index].isRecent,
+          );
+        },
+      ),
+    );
+  }
+
+  // ===== BUILD VIEWED UPDATES HERE =====
+  Widget _buildViewed() {
+
+    List<Status> isViewedList =  Status.getAllStatus.where((status) => status.isViewed == true).toList();
+    int length = isViewedList.length;
+
+    return Column(
+      children: List<Widget>.generate(
+        length,
+        (index) {
+          return StatusItem(
+            contactName: isViewedList[index].contactName,
+            time: isViewedList[index].time,
+            // image is empty for now
+            imagePath: isViewedList[index].imagePath,
+            isViewed: isViewedList[index].isViewed,
+            isRecent: isViewedList[index].isRecent,
+          );
+        },
+      ),
+    );
+  }
+
+  // ===== BUILD MUTED UPDATES HERE =====
+  Widget _buildMuted() {
+
+    List<Status> isMutedList =  Status.getAllStatus.where((status) => status.isViewed == false && status.isRecent == false).toList();
+    int length = isMutedList.length;
+
+    return Column(
+      children: List<Widget>.generate(
+        length,
+            (index) {
+          return StatusItem(
+            contactName: isMutedList[index].contactName,
+            time: isMutedList[index].time,
+            // image is empty for now
+            imagePath: isMutedList[index].imagePath,
+            isViewed: isMutedList[index].isViewed,
+            isRecent: isMutedList[index].isRecent,
+          );
+        },
+      ),
     );
   }
 }
